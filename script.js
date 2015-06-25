a775d87a41f00b092e30800c4f34a8289ff1bcb8
Collin Webb <collindwebb@gmail.com>
2015-06-25 12:34:19 -0700

    makes writable and readable posts

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 'use strict';
 
-angular.module('heimdall', [])
+angular.module('notReddit', [])
 .controller('MainCtrl', function($scope){
+  $scope.links = [{title: "test", url: "test url", description: "test description"}];
+  $scope.addLink = function (){
+    $scope.links.unshift($scope.link);
+    $scope.link = {};
+  };
 });
